614be9239d562c932990faebab91fa8870eed968
Kevin Lowe <iamkevinlowe@gmail.com>
2016-01-01 11:06:55 -0800

    Updated README and fixed some bugs

--- lib/attr_tracker/engine.rb
+++ lib/attr_tracker/engine.rb
@@ -2,6 +2,8 @@ module AttrTracker
   class Engine < ::Rails::Engine
     isolate_namespace AttrTracker
 
+    # So you don't need 'rake attr_tracker:install:migrations'
+    # Just call 'rake db:migrate' as normal
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
